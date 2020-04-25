77f3ee9c1c93dcae9163e326d72d303ccfc26853
Bradley Showen <bshowen@me.com>
2020-04-25 17:32:47 -0400

    initial search results form complete

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,7 +2,7 @@ class FlightsController < ApplicationController
     def index
         @flight_dates ||= Flight.all.order(:departure_date).distinct.pluck(:departure_date).map! {|date| [date.strftime("%B %d %Y"), date] }
         @airports = Airport.all.map{ |f| [f.city.to_s + ", " + f.state + " - " + f.code.to_s, f.id] }
-
+        @flights = Flight.paginate(page: params[:page], per_page: 15).order(:departure_date)
         if params[:flight_data]
             @matching_flights = Flight.where(
                                                                         departure_date:           search_params[:departure_date], 
@@ -10,7 +10,6 @@ class FlightsController < ApplicationController
                                                                         arrival_airport_id:         search_params[:arrival_airport_id] 
                                                                         )
         end
-        byebug
     end
 
     def show
