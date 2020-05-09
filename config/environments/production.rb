b7ca29fcdd52119ba94597dbb40cbc3dbea85ded
Bradley Showen <bshowen@me.com>
2020-05-09 09:44:11 -0400

    refactors production email settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,7 +117,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'flight-booker.com',
+    :domain => 'gmail.com',
     :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
