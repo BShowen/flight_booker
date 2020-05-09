14c1eeaf87f4a0dfda94f3e387afd1fd5ea1774f
Bradley Showen <bshowen@me.com>
2020-05-09 12:13:28 -0400

    updates default from email address

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: ENV["GMAIL_USER_NAME"]
+  # default from: ENV["GMAIL_USER_NAME"]
+  default from: "no-reply@aerosplat.com"
   layout 'mailer'
 end
